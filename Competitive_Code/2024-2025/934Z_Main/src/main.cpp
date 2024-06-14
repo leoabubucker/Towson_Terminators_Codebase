8bac75db525ceae116078343b01d5f16cc628fdd
Leo Abubucker <leokumar@outlook.com>
2024-06-14 10:29:13 -0400

    added MotorCollection functions/implementation; added manual arm control

--- Competitive_Code/2024-2025/934Z_Main/src/main.cpp
+++ Competitive_Code/2024-2025/934Z_Main/src/main.cpp
@@ -9,6 +9,8 @@
 
 #include "vex.h"
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 using namespace vex;
@@ -35,26 +37,19 @@ motor_group nonDriveMotors = motor_group(chainIntake, leftArm, rightArm);
 // motor_group intakeMotors = motor_group(flexwheelIntake, chainIntake);
 motor_group armMotors = motor_group(leftArm, rightArm);
 drivetrain robotDrive = drivetrain(leftDriveMotors, rightDriveMotors, 220, 317.5, 317.5, vex::distanceUnits::mm, 0.75); //vex::drivetrain::drivetrain	(	motor_group leftMotors, motor_group rightMotors, double	wheelTravel = wheel circumference, double trackWidth = middle of left wheel to middle of right wheel, double 	wheelBase = middle of front wheel to middle of back wheel, distanceUnits unit = distanceUnits::mm, double	externalGearRatio = 36:48 (driven gear:driver gear))		
- 
-//NON FUNCTIONAL AUTON
 
-// void drive(int inches, std::string direction){
-//   if(direction == "fwd"){
-//     robotDrive.driveFor(vex::directionType::fwd, inches, vex::distanceUnits::in);
-//   }
-//   else{
-//     robotDrive.driveFor(vex::directionType::rev, inches, vex::distanceUnits::in);
-//   }
-// }
 
-// void turn(int degrees, std::string direction){
-//   if(direction == "left"){
-//     robotDrive.turnFor(vex::turnType::left, degrees, vex::rotationUnits::deg);
-//   }
-//   else{
-//     robotDrive.turnFor(vex::turnType::right, degrees, vex::rotationUnits::deg);
-//   }
-// }
+/*------------------------------------------------------------------------------------*/
+/*                                                                                    */
+/*                              CUSTOM CODE                                           */
+/*                                                                                    */
+/*  Code that is not the acutal functions that are run by VEX competition control.    */
+/*  Includes:                                                                         */
+/*  - Auton Turn function (based on hard-coded values)                                */
+/*  - Auton Drive function (based on hard-coded values)                               */
+/*  - PID Control                                                                     */
+/*  - Auton Drive function (based on hard-coded values)                               */
+/*------------------------------------------------------------------------------------*/
 
 void turn(int degrees, std::string direction, int velocity){
     //Constants determined through testing
@@ -101,35 +96,13 @@ void drive(int inches, std::string direction, int velocity){
     rightFront.spinFor(vex::directionType::fwd, motorDegrees, vex::rotationUnits::deg, velocity, vex::velocityUnits::pct, false);
     rightBack.spinFor(vex::directionType::fwd, motorDegrees, vex::rotationUnits::deg, velocity, vex::velocityUnits::pct, true);
 }
+
+
+
 /*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
+/*                        PID CONTROL                                        */
 /*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
-
-void pre_auton(void) {
-  allMotors.setMaxTorque(100, vex::percentUnits::pct);
-  allMotors.setVelocity(100, vex::percentUnits::pct);
-  armMotors.setVelocity(50, vex::velocityUnits::pct);
-  allMotors.setTimeout(5, vex::timeUnits::sec);
-  nonDriveMotors.setStopping(vex::brakeType::hold);
-  robotDrive.setTurnVelocity(50, vex::velocityUnits::pct);
-  allMotors.resetPosition();
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
-}
-
-template <typename T>
-std::string to_string(T value)
-{
-    std::ostringstream os ;
-    os << value ;
-    return os.str() ;
-}
 
 // Constant Settings
 double kP = 1.0;
@@ -223,6 +196,119 @@ int drivePID(){
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
+/*                           Motor Collection Class                          */
+/*                                                                           */
+/*  Implementation of MotorCollection class w/                               */
+/*  the following functions (function names abbreviated w/o parameters):     */
+/*      void MotorCollection::addMotor() - adds the passed in motor object   */
+/*          and std::string object motorName to the motorList and            */
+/*          motorNamesList respectively.                                     */
+/*      std::vector<bool> MotorCollection::isConnected() - Returns a list    */
+/*          of boolean variables representing whether the motor is           */
+/*          connected or disconnected. If motor is disconnected, prints      */
+/*          the motor name on the Controller screen and rumbles it.          */ 
+/*      std::string MotorCollection::checkDriveMotors() - Returns a          */
+/*          std::string var representing the drive configuration as          */
+/*          determined by which drive motors are connected/disconnected.     */
+/*      std::vector<std::string> MotorCollections:returnPositions(),         */
+/*      returnVelocities(), returnTorque(), returnTemperatures() -           */
+/*          Returns a list of motor pos/vel/torq/temp in the format          */
+/*          "name pos/vel/torq/temp"                                         */            
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+
+class MotorCollection{
+  public:
+    std::vector<vex::motor> motorList; //Init vector of motor objects
+    std::vector<std::string> motorNamesList; //Init vector of std::strings representing motor names
+    
+    void addMotor(motor newMotor, std::string newMotorName){
+        motorList.push_back(newMotor);
+        motorNamesList.push_back(newMotorName);
+    }
+    std::vector<bool> isConnected(){
+      Controller1.Screen.clearScreen();
+        std::vector<bool> motorConnections;
+        for(int i=0; i<motorList.size(); i++){
+            motorConnections.push_back(motorList[i].installed());
+            if(!motorList[i].installed()){
+                Controller1.Screen.print(motorNamesList[i].c_str());
+            }
+        } 
+        return motorConnections;
+    }
+
+    std::string checkMotors(){
+        std::vector<bool>motorConnections = isConnected();
+        std::vector<std::string> disconnectedMotorNames;
+        std::string updatedDriveConfig = "fourWheel";
+        for(int i=0; i<motorConnections.size(); i++){
+           if(motorConnections[i] == false){
+                disconnectedMotorNames.push_back(motorNamesList[i]);
+           }
+        }
+        if(std::find(disconnectedMotorNames.begin(), disconnectedMotorNames.end(), "LF") != disconnectedMotorNames.end() || std::find(disconnectedMotorNames.begin(), disconnectedMotorNames.end(), "RF") != disconnectedMotorNames.end()){
+            updatedDriveConfig = "rearWheel";
+        }
+
+        if(std::find(disconnectedMotorNames.begin(), disconnectedMotorNames.end(), "LB") != disconnectedMotorNames.end() || std::find(disconnectedMotorNames.begin(), disconnectedMotorNames.end(), "RB") != disconnectedMotorNames.end()){
+            updatedDriveConfig = "frontWheel";
+        }
+
+        if(std::find(disconnectedMotorNames.begin(), disconnectedMotorNames.end(), "LF") != disconnectedMotorNames.end() && std::find(disconnectedMotorNames.begin(), disconnectedMotorNames.end(), "RB") != disconnectedMotorNames.end()){
+            updatedDriveConfig = "RFLB";
+        }
+        if(std::find(disconnectedMotorNames.begin(), disconnectedMotorNames.end(), "LB") != disconnectedMotorNames.end() || std::find(disconnectedMotorNames.begin(), disconnectedMotorNames.end(), "RF") != disconnectedMotorNames.end()){
+            updatedDriveConfig = "LFRB";
+        }
+        return updatedDriveConfig;
+    }
+};
+
+MotorCollection myMotorCollection;
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  allMotors.setMaxTorque(100, vex::percentUnits::pct);
+  allMotors.setVelocity(100, vex::percentUnits::pct);
+  armMotors.setVelocity(50, vex::velocityUnits::pct);
+  allMotors.setTimeout(5, vex::timeUnits::sec);
+  nonDriveMotors.setStopping(vex::brakeType::hold);
+  robotDrive.setTurnVelocity(50, vex::velocityUnits::pct);
+  allMotors.resetPosition();
+
+  myMotorCollection.addMotor(leftArm, "LA");
+  myMotorCollection.addMotor(rightArm, "RA");
+  myMotorCollection.addMotor(leftBack, "LB");
+  myMotorCollection.addMotor(leftFront, "LF");
+  myMotorCollection.addMotor(rightBack, "RB");
+  myMotorCollection.addMotor(rightFront, "RF");
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+template <typename T>
+std::string to_string(T value)
+{
+    std::ostringstream os ;
+    os << value ;
+    return os.str() ;
+}
+
+
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
 /*                              Autonomous Task                              */
 /*                                                                           */
 /*  This task is used to control your robot during the autonomous phase of   */
@@ -259,21 +345,45 @@ void autonomous(void) {
 
 void usercontrol(void) {
   enableDrivePID = false;
+  std::string driveConfig = myMotorCollection.checkMotors(); //Checks motor statuses and switches drive mode (4-wheel, front-wheel, rear-wheel, LFRB, RFLB)
   // User control code here, inside the loop
   while (1) {
     //Drive Controls
-    leftFront.spin(vex::directionType::fwd, Controller1.Axis3.position(), vex::velocityUnits::pct);
-     leftBack.spin(vex::directionType::fwd, Controller1.Axis3.position(), vex::velocityUnits::pct);
-    rightFront.spin(vex::directionType::fwd, Controller1.Axis2.position(), vex::velocityUnits::pct); 
-    rightBack.spin(vex::directionType::fwd, Controller1.Axis2.position(), vex::velocityUnits::pct); 
+    if(driveConfig == "rearWheel"){
+      leftBack.spin(vex::directionType::fwd, Controller1.Axis3.position(), vex::velocityUnits::pct);
+      rightBack.spin(vex::directionType::fwd, Controller1.Axis2.position(), vex::velocityUnits::pct); 
+    }
+    else if(driveConfig == "frontWheel"){
+      leftFront.spin(vex::directionType::fwd, Controller1.Axis3.position(), vex::velocityUnits::pct);
+      rightFront.spin(vex::directionType::fwd, Controller1.Axis2.position(), vex::velocityUnits::pct); 
+    }
+    else if(driveConfig == "RFLB"){
+      rightFront.spin(vex::directionType::fwd, Controller1.Axis3.position(), vex::velocityUnits::pct);
+      leftBack.spin(vex::directionType::fwd, Controller1.Axis2.position(), vex::velocityUnits::pct);     
+    }
+    else if(driveConfig == "LFRB"){
+      leftFront.spin(vex::directionType::fwd, Controller1.Axis3.position(), vex::velocityUnits::pct);
+      rightBack.spin(vex::directionType::fwd, Controller1.Axis2.position(), vex::velocityUnits::pct);     
+    }
+    else{
+      leftFront.spin(vex::directionType::fwd, Controller1.Axis3.position(), vex::velocityUnits::pct);
+      leftBack.spin(vex::directionType::fwd, Controller1.Axis3.position(), vex::velocityUnits::pct);
+      rightFront.spin(vex::directionType::fwd, Controller1.Axis2.position(), vex::velocityUnits::pct); 
+      rightBack.spin(vex::directionType::fwd, Controller1.Axis2.position(), vex::velocityUnits::pct); 
+    }
 
     //Testing Degree Values for Drive
-    Brain.Screen.print(leftFront.position(vex::rotationUnits::deg) + leftBack.position(vex::rotationUnits::deg) + rightFront.position(vex::rotationUnits::deg) + rightBack.position(vex::rotationUnits::deg));
+    // Brain.Screen.print(leftFront.position(vex::rotationUnits::deg) + leftBack.position(vex::rotationUnits::deg) + rightFront.position(vex::rotationUnits::deg) + rightBack.position(vex::rotationUnits::deg));
    
     //Testing Degree Values for Arm
-    //Brain.Screen.print(armMotors.position(vex::rotationUnits::deg));
-    if(Controller1.ButtonUp.pressing()){
+    Brain.Screen.print(armMotors.position(vex::rotationUnits::deg));
+    
+    if(Controller1.ButtonB.pressing()){
       Brain.Screen.clearScreen();
+    }
+
+    if(Controller1.ButtonX.pressing()){
+      driveConfig = myMotorCollection.checkMotors();
     }
 
     //Intake Controls
@@ -288,34 +398,51 @@ void usercontrol(void) {
     }
 
     //Arm Controls
-    if(Controller1.ButtonR1.pressing()){
-      if(armMotors.position(vex::rotationUnits::deg) < 200){
-        armMotors.spinToPosition(200, vex::rotationUnits::deg);
+    if(Controller1.ButtonUp.pressing()){
+      if(Controller1.ButtonR1.pressing()){
+        armMotors.spin(vex::directionType::fwd);
       }
-      else if(armMotors.position(vex::rotationUnits::deg) < 400){
-        armMotors.spinToPosition(400, vex::rotationUnits::deg);
+      else if(Controller1.ButtonR2.pressing()){
+        armMotors.spin(vex::directionType::rev);
       }
-      else if(armMotors.position(vex::rotationUnits::deg) < 600){
-        armMotors.spinToPosition(600, vex::rotationUnits::deg);
+      else{
+        armMotors.stop();
       }
-      else if(armMotors.position(vex::rotationUnits::deg) < 800){
-        armMotors.spinToPosition(800, vex::rotationUnits::deg);
+    }
+    else{
+      if(Controller1.ButtonR1.pressing()){
+        if(armMotors.position(vex::rotationUnits::deg) < 200){
+          armMotors.spinToPosition(200, vex::rotationUnits::deg);
+        }
+        else if(armMotors.position(vex::rotationUnits::deg) < 400){
+          armMotors.spinToPosition(400, vex::rotationUnits::deg);
+        }
+        else if(armMotors.position(vex::rotationUnits::deg) < 600){
+          armMotors.spinToPosition(600, vex::rotationUnits::deg);
+        }
+        else if(armMotors.position(vex::rotationUnits::deg) < 800){
+          armMotors.spinToPosition(800, vex::rotationUnits::deg);
+        }
+      } 
+      else if(Controller1.ButtonR2.pressing()){
+        if(armMotors.position(vex::rotationUnits::deg) >=800){
+          armMotors.spinToPosition(600, vex::rotationUnits::deg);
+        }
+        else if(armMotors.position(vex::rotationUnits::deg) >= 600){
+          armMotors.spinToPosition(400, vex::rotationUnits::deg);
+        }
+        else if(armMotors.position(vex::rotationUnits::deg) >= 400){
+          armMotors.spinToPosition(200, vex::rotationUnits::deg);
+        }
+        else if(armMotors.position(vex::rotationUnits::deg) >= 200){
+          armMotors.spinToPosition(0, vex::rotationUnits::deg);
+        }
       }
-    } 
-    else if(Controller1.ButtonR2.pressing()){
-      if(armMotors.position(vex::rotationUnits::deg) >=800){
-        armMotors.spinToPosition(600, vex::rotationUnits::deg);
+      else{
+        armMotors.stop();
       }
-      else if(armMotors.position(vex::rotationUnits::deg) >= 600){
-        armMotors.spinToPosition(400, vex::rotationUnits::deg);
-      }
-      else if(armMotors.position(vex::rotationUnits::deg) >= 400){
-        armMotors.spinToPosition(200, vex::rotationUnits::deg);
-      }
-      else if(armMotors.position(vex::rotationUnits::deg) >= 200){
-        armMotors.spinToPosition(0, vex::rotationUnits::deg);
-      }
-    }    
+    }
+    
     
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
