d96efa674a55aa7746206d1433cb8ce6b3a643ef
Daksh-Sangar <81648617+Daksh-Sangar@users.noreply.github.com>
2025-03-03 22:35:59 -0500

    934A auton update and code cleanup

--- Competitive_Code/2024-2025/934A Pros/src/main.cpp
+++ Competitive_Code/2024-2025/934A Pros/src/main.cpp
@@ -1,24 +1,31 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
 
+
 //controller
 pros::Controller controller (pros::E_CONTROLLER_MASTER);
+
 
 //motors
 pros::Motor interalIntake(-5, pros::v5::MotorGears::green);
 pros::Motor externalIntake(6, pros::v5::MotorGears::green);
 pros::MotorGroup rightMotors({3, 11}, pros::v5::MotorGears::green);
 pros::MotorGroup leftMotors({-1, -2}, pros::v5::MotorGears::green);
+pros::Motor fishMech(20, pros::v5::MotorGears::green);
+
 
 //inertial sensor (port 10)
-pros::Imu inertial(10); 
+pros::Imu inertial(10);
+
 
 pros::adi::DigitalOut clamp('A');
+
 
 //drivetrain settings
 lemlib::Drivetrain drivetrain{&leftMotors, &rightMotors, 13, lemlib::Omniwheel::OLD_275, 266 , 2};
 //sensors for odom
 lemlib::OdomSensors sensors(nullptr, nullptr, nullptr, nullptr, &inertial);
+
 
 // lateral PID controller
 lemlib::ControllerSettings lateral_controller(80, // proportional gain (kP)
@@ -32,10 +39,11 @@ lemlib::ControllerSettings lateral_controller(80, // proportional gain (kP)
                                               127 // maximum acceleration (slew)
 );
 
+
 // angular PID controller
-lemlib::ControllerSettings angular_controller(15, // proportional gain (kP) 
+lemlib::ControllerSettings angular_controller(15, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              55, // derivative gain (kD) 
+                                              55, // derivative gain (kD)
                                               0, // anti windup 252.1965
                                               1, // small error range, in inches
                                               100, // small error range timeout, in milliseconds
@@ -44,14 +52,19 @@ lemlib::ControllerSettings angular_controller(15, // proportional gain (kP)
                                               0 // maximum acceleration (slew)
 );
 
+
 //input curve for throttle input during driver control (deadband, min output required, expo curve gain)
 lemlib::ExpoDriveCurve throttle_curve(3, 10, 1.019);
+
 
 // input curve for steer input during driver control
 lemlib::ExpoDriveCurve steer_curve(3, 10, 1.019);
 
+
 // create the chassis
 lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller,  sensors, &throttle_curve, &steer_curve);
+
+
 
 
 /**
@@ -61,11 +74,12 @@ lemlib::Chassis chassis(drivetrain, lateral_controller, angular_controller,  sen
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	
-	pros::lcd::initialize(); // initialize brain screen
-	chassis.calibrate(); // calibrate sensor
+   
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensor
 
-	//REMOVE, USED FOR AUTON TESTING
+
+    //REMOVE, USED FOR AUTON TESTING
     // print position to brain screen
     pros::Task screen_task([&]() {
         while (true) {
@@ -79,12 +93,14 @@ void initialize() {
     });
 }
 
+
 /**
  * Runs while the robot is in the disabled state of Field Management System or
  * the VEX Competition Switch, following either autonomous or opcontrol. When
  * the robot is enabled, this task will exit.
  */
 void disabled() {}
+
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
@@ -96,6 +112,7 @@ void disabled() {}
  * starts.
  */
 void competition_initialize() {}
+
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -110,88 +127,97 @@ void competition_initialize() {}
  */
 void autonomous() {
 
-	//head to head auton 1  (3 goal side)
-	chassis.setPose(0,0,0);
-	clamp.set_value(true);
-	chassis.moveToPoint(0, -26, 3000, {.forwards = false});
+    // DRIVER NOTE REMOVE LATER
+    // SETUP AUTON WITH CLAMP BRACKET LEFT SIDE PARRALEL TO 1ST REAL SQUARE OF TILE AND PUSH A LITTLE BACK 
+   
+    //HEAD TO HEAD BLUE RIGHT SIDE (RING RUSH)
+   /*
+    chassis.setPose(0,0,0);
+    clamp.set_value(true);
+    pros::delay(500);
+    chassis.moveToPoint(0, -24, 3000, {.forwards = false, .maxSpeed = 60}, false);
+    clamp.set_value(false);
+    pros::delay(1000);
+    interalIntake.move(127);
+    externalIntake.move(127);
+    pros::delay(700);
+    chassis.turnToHeading(290, 1000);
+    chassis.moveToPoint(-23, -18, 2000, {.forwards = true}, false);
 	pros::delay(500);
-	clamp.set_value(false);
-	pros::delay(1000);
-	interalIntake.move(127);
-	chassis.turnToHeading(-90);
-	chassis.moveToPoint(-24, -26, 3000, {.forwards = true});
-	pros::delay(3000);
-	interalIntake.move(0);
-	
-	//chassis.moveToPoint(0, -5, 1000, {.forwards = false});
-	//chassis.turnToHeading(90, 2000);
-	//externalIntake.move(110);
-	//chassis.moveToPoint(0, 26, 2000, {.forwards = true});
-	//pros::delay(2000);
-	//chassis.moveToPoint(/*test negative*/-26, -26, 2000, {.forwards = false});
-	//interalIntake.move(0);
-	//externalIntake.move(0);
-	
+	chassis.turnToHeading(185, 1000, {.direction = AngularDirection::CCW_COUNTERCLOCKWISE});
+	chassis.moveToPoint(-23, -28, 2000, {.forwards = true}, false);
+	chassis.turnToHeading(195, 1000);
+	*/
 
-	
-	//head to head auton 2 (2 goal side)
-	//chassis.setPose(0,0,0);
-	//chassis.moveToPoint(0, -30, 3000, {.forwards = false});
-	//pros::delay(1000);
-	//clamp.set_value(true);
-	//interalIntake.move(80);
-	//pros::delay(2000);
-	//interalIntake.move(0);
-	//chassis.moveToPoint(0, -5, 1000, {.forwards = false});
-	//chassis.turnToHeading(-90, 2000);
-	//chassis.moveToPoint(0, 26, 2000, {.forwards = true});
-	//externalIntake.move(110);
-	//pros::delay(2000);
-	//interalIntake.move(0);
-	//externalIntake.move(0);
-	//chassis.moveToPoint(-26, /*test negative*/-48, 2000, {.forwards = false});
-	//chassis.moveToPoint(0, 20, 2000, {.forwards = false});
-
-	// programming skills auton 
+	//HEAD TO HEAD BLUE LEFT SIDE
+	/*
 	chassis.setPose(0,0,0);
-	clamp.set_value(true);
-	chassis.moveToPoint(0, -2, 3000, {.forwards = false});
-	pros::delay(1000);
-	clamp.set_value(false);
-	chassis.turnToHeading(180, 2000);
-	interalIntake.move(127);
-	externalIntake.move(127);
-	chassis.moveToPoint(0, -30, 3000, {.forwards = true});
-	chassis.turnToHeading(90, 2000);
-	chassis.moveToPoint(24, -35, 3000, {.forwards = true});
-	chassis.turnToHeading(90, 2000);
-	//last 2 rings 
-	chassis.moveToPoint(24, 0, 3000, {.forwards = true});
-	//test
-	chassis.moveToPoint(32, -5, 2000, {.forwards = true});
-	chassis.moveToPoint(45, 8, 2000, {.forwards = false});
-	pros::delay(1000);
-	clamp.set_value(true);
-	//end of left side; move to next goal (moveToPose)
-	chassis.moveToPoint(/*test*/-48, -5, 5000, {.forwards = false});
-	pros::delay(1000);
-	clamp.set_value(false);
-	chassis.turnToHeading(-90, 2000);
-	chassis.moveToPoint(-48, -30, 3000, {.forwards = true});
-	chassis.turnToHeading(-90, 2000);
-	chassis.moveToPoint(-72, -30, 3000, {.forwards = true});
-	chassis.turnToHeading(-90, 2000);
-	chassis.moveToPoint(-72, 0, 3000, {.forwards = true});
-	//test
-	chassis.moveToPoint(-72, -6, 2000, {.forwards = true});
-	chassis.moveToPoint(-94, 8, 2000, {.forwards = false});
-	pros::delay(1000);
-	clamp.set_value(true);
-	interalIntake.move(0);
-	externalIntake.move(0);
-	//end of right side
+    clamp.set_value(true);
+    pros::delay(500);
+    chassis.moveToPoint(0, -24, 3000, {.forwards = false, .maxSpeed = 60}, false);
+	chassis.moveToPoint(0, -28, 1000, {.forwards = false}, true);
+    clamp.set_value(false);
+    pros::delay(1000);
+    interalIntake.move(127);
+    externalIntake.move(127);
+    pros::delay(700);
+    chassis.turnToHeading(90, 1000);
+    chassis.moveToPoint(23, -18, 2000, {.forwards = true}, false);
+	*/
+
+    ///CURRENT WORKING AUTON
+    
+    //chassis.setPose(0, 0, 0);
 	
+    interalIntake.move(127);
+    pros::delay(1500);
+    interalIntake.move(0);
+	clamp.set_value(true);
+    chassis.moveToPoint(0, 8, 3000, {.forwards = true}, false);
+    chassis.turnToHeading(270, 3000, {.direction = AngularDirection::CCW_COUNTERCLOCKWISE}, false);
+	chassis.moveToPoint(15, 8, 1000, {.forwards = false, .maxSpeed = 80}, false);
+	chassis.moveToPoint(18, 8, 1000, {.forwards = false}, true);
+	clamp.set_value(false);
+	externalIntake.move(127);
+	interalIntake.move(127);
+	pros::delay(1000);
+	chassis.moveToPoint(65, 8, 4000, {.forwards = true}, false);
+    //chassis.moveToPoint(45, -10, 2000, {.forwards = false}, false);
+    //chassis.turnToHeading(320, 3000, {.direction = AngularDirection::CCW_COUNTERCLOCKWISE}, false);
+    //chassis.moveToPoint(45, -25, 1000,{.forwards = false}, false);
+    //chassis.moveToPoint(0, 14, 3000, {.forwards = true}, false);
+    //chassis.turnToHeading(50, 3000, {}, false);
+    //chassis.moveToPoint(-25, 2, 3000, {.forwards = false}, false);
+    //chassis.moveToPoint(-35, -7, 3000, {.forwards = false}, false);
+    //chassis.turnToHeading(140, 3000, {}, false);
+    //chassis.moveToPoint(-80, 26, 3000, {.forwards = false}, false);
+    //chassis.turnToHeading(180, 3000, {}, false);
+    //chassis.moveToPoint(-80, 50 , 3000, {.forwards = false}, false);
+    
+   
+   
+    //chassis.moveToPoint(0, 0,  3000, {.forwards = false}, false);
+   
+    //chassis.moveToPoint(40, 5, 3000,{.forwards = true}, false);
+    //chassis.turnToHeading(50, 2000);
+    //chassis.moveToPoint(-79, -20, 5000, {.forwards = false}, false);
+   
+   
+    //chassis.moveToPose(11,12,-55, 2000);  
+    //chassis.turnToHeading(-55, 2000);
+    //chassis.moveToPoint(48, 0, 3000, {.forwards = false});
+    //chassis.moveToPose(38, -16, -29, 3000);
+    //chassis.moveToPoint(0, -5, 1000, {.forwards = false});
+    //chassis.turnToHeading(90, 2000);
+    //externalIntake.move(110);
+    //chassis.moveToPoint(0, 26, 2000, {.forwards = true});
+    //pros::delay(2000);
+    //chassis.moveToPoint(/*test negative*/-26, -26, 2000, {.forwards = false});
+    //interalIntake.move(0);
+    //externalIntake.move(0);
+   
 }
+
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -207,47 +233,64 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	
-	//open clamp at start of user control
-	clamp.set_value(true);
+   
+    while (true) {
 
-	while (true) {
 
-		/*		DRIVE CONTROLS
-			Left joystick for forward and backward
-			RIght joystick for turning
-		*/
-		int rightJoystickTurn = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-		int leftJoysticForward = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        /*      DRIVE CONTROLS
+            Left joystick for forward and backward
+            RIght joystick for turning
+        */
+        int rightJoystickTurn = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        int leftJoysticForward = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 
-		chassis.arcade(leftJoysticForward, rightJoystickTurn, false, 0.6);
 
-		/*		INTAKE CONTROLS
-			R1 and R2 for forward
-			L1 and L2 for reverse
-		*/
-		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)||controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)){
-			interalIntake.move(127);
-			externalIntake.move(127);
-		}else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) || controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
-		{
-			interalIntake.move(-127); //-80
-			externalIntake.move(-127); //-110
-		}else{
-			interalIntake.move(0);
-			externalIntake.move(0);
-		}
+        chassis.arcade(leftJoysticForward, rightJoystickTurn, false, 0.6);
 
-		/*		CLAMP CONTROLS
-			X to extend (open)
-			B to retract (close)
-		*/
-		if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_X)){
-			clamp.set_value(true);
-		}else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)){
-			clamp.set_value(false);
-		}
 
-		pros::delay(20);                             
-	}
+        /*      INTAKE CONTROLS
+            R1 and R2 for forward
+            L1 and L2 for reverse
+        */
+        if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)||controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)){
+            interalIntake.move(127);
+            externalIntake.move(127);
+        }else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1) || controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+        {
+            interalIntake.move(-127); //-80
+            externalIntake.move(-127); //-110
+        }else{
+            interalIntake.move(0);
+            externalIntake.move(0);
+        }
+
+
+        /*      CLAMP CONTROLS
+            X to extend (open)
+            B to retract (close)
+        */
+        if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_X)){
+            clamp.set_value(true);
+        }else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_B)){
+            clamp.set_value(false);
+        }
+
+
+        /*FISH MECH*/
+
+        if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_UP)){
+            fishMech.move(127);
+        }else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)){
+            fishMech.move(-127);
+        }else{
+            fishMech.move(0);
+
+
+        }
+
+
+        pros::delay(20);                            
+    }
 }
+
+
